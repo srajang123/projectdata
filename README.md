3cc998cd5ebad3ed47946fd2eace6ac098246f53
SRAJAN GUPTA <srajan@ubuntu.ubuntu-domain>
2019-11-24 19:59:19 +0530

    Cleared conflicts while updating node version

--- README.md
+++
@@ -1,2 +0,0 @@
-ANSWER HOME 
-This contains the project made for innotech. Students ask he queries which are answered by the teachers
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,6 +1,6 @@
-const reset=document.querySelector('#reset');
-const submit=document.querySelector('#submit');
-reset.addEventListener('click',(e)=>{
-    if(!confirm('Do You want to reset?'))
+const reset = document.querySelector('#reset');
+const submit = document.querySelector('#submit');
+reset.addEventListener('click', (e) => {
+    if (!confirm('Do You want to reset?'))
         e.preventDefault();
 });
